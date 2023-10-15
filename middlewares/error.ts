e39914ee5e196f06ba00f1fe35332bf6fd71d284
promehedi <admin@promehedi.com>
2023-10-16 00:34:34 +0600

    Improve Error Middleware

--- middlewares/error.ts
+++ middlewares/error.ts
@@ -2,6 +2,17 @@ import Elysia from 'elysia'
 
 export const error = () =>
   new Elysia().onError((c) => {
+    if (c.code === 'NOT_FOUND') {
+      c.set.status = 501
+      return {
+        success: false,
+        status: 501,
+        message: `Not Found - [${c.request.method}]:[${c.path}]`,
+        stack: process.env.NODE_ENV === 'production' ? null : c.error?.stack,
+      }
+    }
+
+    c.set.status = c.set.status || 500
     return {
       success: false,
       status: c.set.status,
